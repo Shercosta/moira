43c254df0cff0125cecb196d1f18399e21981450
Shercosta <shercostagp@gmail.com>
2026-07-30 13:08:20 +0700

    feat: hero mobile

--- src/components/local/Hero.tsx
+++ src/components/local/Hero.tsx
@@ -22,55 +22,64 @@ function HeroPersonPicture({
 
 export function Hero() {
   return (
-    <div id="hero" className="flex justify-center pb-10">
-      <div className="relative">
+    <section
+      id="hero"
+      className="flex justify-center px-6 pb-12 md:px-8 md:pb-10"
+    >
+      {/* Left decorations (desktop only) */}
+      <div className="relative hidden md:block">
         <HeroPersonPicture
           imgPath="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/hero-person-1.jpg"
-          className="absolute w-20 right-20"
+          className="absolute right-20 w-20"
         />
         <HeroPersonPicture
           imgPath="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/hero-person-1.jpg"
-          className="absolute w-16 right-40 top-35"
+          className="absolute top-35 right-40 w-16"
         />
         <HeroPersonPicture
           imgPath="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/hero-person-1.jpg"
-          className="absolute w-13 right-10 top-55"
+          className="absolute top-55 right-10 w-13"
         />
       </div>
-      <div className="w-[50%]">
-        <div className="mb-3">
-          <h1 className="text-center text-5xl font-normal text-primary capitalize">
+
+      {/* Content */}
+      <div className="w-full md:w-[50%]">
+        <div className="mb-4">
+          <h1 className="text-center text-4xl font-normal capitalize text-primary md:text-5xl">
             Lorem ipsum dolor sit amet consectetur
           </h1>
         </div>
-        <div className="max-w-[70%] mx-auto">
-          <p className="text-center text-primary">
+
+        <div className="mx-auto max-w-full md:max-w-[70%]">
+          <p className="text-center text-sm leading-7 text-primary md:text-base">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque
             quidem culpa iste repellat blanditiis quos reiciendis fugiat.
             Expedita, nam assumenda.
           </p>
         </div>
 
-        <div className="flex justify-center mt-10">
-          <Button className="rounded-full font-thin px-5">
+        <div className="mt-8 flex justify-center md:mt-10">
+          <Button className="rounded-full px-6 font-thin md:px-5">
             Lorem ipsum dolor
           </Button>
         </div>
       </div>
-      <div className="relative">
+
+      {/* Right decorations (desktop only) */}
+      <div className="relative hidden md:block">
         <HeroPersonPicture
           imgPath="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/hero-person-1.jpg"
-          className="absolute w-20 left-20"
+          className="absolute left-20 w-20"
         />
         <HeroPersonPicture
           imgPath="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/hero-person-1.jpg"
-          className="absolute w-16 left-40 top-35"
+          className="absolute top-35 left-40 w-16"
         />
         <HeroPersonPicture
           imgPath="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/hero-person-1.jpg"
-          className="absolute w-13 left-10 top-55"
+          className="absolute top-55 left-10 w-13"
         />
       </div>
-    </div>
+    </section>
   );
 }

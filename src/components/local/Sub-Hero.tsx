442f6b04bf21f26dd838d45bae8715b48ae0aef4
Shercosta <shercostagp@gmail.com>
2026-07-30 16:48:47 +0700

    feat: photo fill sub hero

--- src/components/local/Sub-Hero.tsx
+++ src/components/local/Sub-Hero.tsx
@@ -4,17 +4,17 @@ export function SubHero() {
       <img
         src="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/tech-meeting-flatlay.jpg"
         alt="tech-meeting-flatlay.jpg"
-        className="aspect-5/4 w-60 rounded-lg rounded-tr-none"
+        className="object-fill aspect-5/4 w-60 rounded-lg rounded-tr-none"
       />
       <img
         src="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/sample-sub-hero.jpg"
         alt="sample-sub-hero.jpg"
-        className="aspect-5/4 w-60 rounded-lg"
+        className="object-fill aspect-5/4 w-60 rounded-lg"
       />
       <img
         src="https://raw.githubusercontent.com/Shercosta/moira/refs/heads/master/public/sample-sub-hero0.jpg"
         alt="sample-sub-hero0.jpg"
-        className="aspect-5/4 w-60 rounded-lg rounded-tl-none"
+        className="object-fill aspect-5/4 w-60 rounded-lg rounded-tl-none"
       />
     </div>
   );

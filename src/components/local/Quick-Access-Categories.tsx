bd22686f8f4fc80bc83ceaec6db1653a7fc15ac0
Shercosta <shercostagp@gmail.com>
2026-07-31 13:14:52 +0700

    feat: quick access

--- src/components/local/Quick-Access-Categories.tsx
+++ src/components/local/Quick-Access-Categories.tsx
@@ -1,8 +1,9 @@
 import { Rocket } from "lucide-react";
+import { Button } from "../ui/button";
 
 function QuickAccessBox() {
   return (
-    <div className="rounded-lg bg-white p-4 max-w-80 m-2">
+    <div className="rounded-lg bg-white p-4 max-w-80 m-3">
       <div className="rounded-full bg-primary text-white p-2 flex w-min">
         <Rocket size={20} />
       </div>
@@ -12,6 +13,10 @@ function QuickAccessBox() {
       <p className="text-sm">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </p>
+
+      <Button className="rounded-full px-6 font-thin md:px-5 mt-3">
+        Explore Our Support
+      </Button>
     </div>
   );
 }

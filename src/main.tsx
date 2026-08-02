8d0d1ad5741dd9e862fbdb9335f727af99d3a88e
Shercosta <shercostagp@gmail.com>
2026-08-02 13:00:59 +0700

    chore: remove unnecessary main wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HeroProvider } from "./context/HeroContext.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { UserLocationProvider } from "./context/UserLocationContext.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <UserLocationProvider>
-        <HeroProvider>
-          <App />
-        </HeroProvider>
-      </UserLocationProvider>
+      <App />
     </BrowserRouter>
     <Toaster />
   </StrictMode>,

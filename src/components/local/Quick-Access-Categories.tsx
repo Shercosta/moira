26d8ebba1542ed8d54e761c3cb3158cca61eedf5
Shercosta <shercostagp@gmail.com>
2026-08-04 20:30:42 +0700

    feat: subtle background adaptable

--- src/components/local/Quick-Access-Categories.tsx
+++ src/components/local/Quick-Access-Categories.tsx
@@ -154,7 +154,7 @@ export function QuickAccessCategories() {
   return (
     <section
       id="quickAccessCategories"
-      className="bg-subtle-dark px-5 py-12 md:px-8 lg:px-16"
+      className="bg-subtle-dark-adaptable px-5 py-12 md:px-8 lg:px-16"
     >
       <div className="mx-auto max-w-4xl">
         <h2 className="text-center font-thin text-muted">

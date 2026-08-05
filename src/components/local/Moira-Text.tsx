37cfdd87b57778075f98ab54c91c2247b78ff476
Shercosta <shercostagp@gmail.com>
2026-08-05 15:41:58 +0700

    feat: spin moira logo

--- src/components/local/Moira-Text.tsx
+++ src/components/local/Moira-Text.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { cn, useNavigateAndScroll } from "../../lib/common";
 
 interface MoiraTextProps {
@@ -6,16 +7,20 @@ interface MoiraTextProps {
 
 export function MoiraText({ className = "" }: MoiraTextProps) {
   const navscroll = useNavigateAndScroll();
+  const [rotation, setRotation] = useState(0);
 
   return (
     <div
-      className={cn("inline-flex items-center gap-2 cursor-pointer", className)}
+      className={cn("inline-flex cursor-pointer items-center gap-2", className)}
       onClick={() => navscroll("/")}
+      onMouseEnter={() => setRotation((prev) => prev + 180)}
+      onMouseLeave={() => setRotation((prev) => prev + 180)}
     >
       <img
         src="/moiraicon.png"
         alt="Moira"
-        className="h-[1em] w-[1em] object-contain"
+        className="h-[1em] w-[1em] object-contain transition-transform duration-500"
+        style={{ transform: `rotate(${rotation}deg)` }}
       />
 
       <span>Moira</span>
